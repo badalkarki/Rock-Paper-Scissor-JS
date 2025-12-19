0dba129c72512d4a438248a892eecfd428de1d20
Badal karki <128877662+badalkarki@users.noreply.github.com>
2025-12-19 21:57:44 +0545

    Creates function to give winner of total games
    Previous code was only suited for one round. So this function just
    plays the round of 5 games and give the overall winner of the game.
    
    Also previously the function were called right after the function block
    but doing that caused problem with the counter of scores so, now
    all the function are called properly inside a playGame function making
    them the one part of the game.

--- script.js
+++ script.js
@@ -3,8 +3,6 @@ function getHumanChoice(userInput){
     userInput = userInput.toLowerCase();
     return userInput;
 }
-const userChoice = getHumanChoice(prompt("Choose between rock,paper and scissor"));
-console.log(userChoice)
 
 // // Function to get the choice of the computer.
 function getComputerChoice(choice){
@@ -16,21 +14,54 @@ function getComputerChoice(choice){
         return "scissor";
     }
 }
-const computerChoice = getComputerChoice(Math.random()*20);
-console.log(computerChoice);
+
+    // variables to count the number of rounds won by Human and Computer
+    let humanScore = 0;
+    let computerScore = 0;
+
+
 
 // Function to compare the choices and select winner for round;
 function playRound(humanChoice, computerChoice){
     if (humanChoice === "rock" && computerChoice === "paper" || humanChoice === "paper" && computerChoice === "scissor" || humanChoice === "scissor" && computerChoice === "rock"){
-
-         return `You lost! ${computerChoice} beats ${humanChoice}`;
+        computerScore += 1;
+         return `You lost! ${computerChoice} beats ${humanChoice}.`;
 
     }else if (humanChoice === computerChoice){
-        return `It's a draw ${computerChoice} is same as ${humanChoice}`
+        return `It's a draw ${computerChoice} is same as ${humanChoice}.`
     }else {
-        return `You won! ${humanChoice} beats ${computerChoice}`
+        humanScore += 1;
+        return `You won! ${humanChoice} beats ${computerChoice}.`
     }
 
 } 
-const roundWinner = playRound(userChoice, computerChoice);
-console.log(roundWinner);
+
+
+// function to play the round of 5 game and decide the winner
+function playGame(){
+
+    for(let i = 0; i < 5; i++){
+        const userChoice = getHumanChoice(prompt("Choose between rock,paper and scissor"));
+        console.log(userChoice)
+
+        const computerChoice = getComputerChoice(Math.random()*20);
+        console.log(computerChoice);
+
+        const roundWinner = playRound(userChoice, computerChoice);
+        console.log(roundWinner);
+    }
+
+    if(humanScore > computerScore){
+        return ("Yeah Humans are still superior than the computers, you proved it.");
+    }else if(computerScore > humanScore) {
+        return ("Oh no! Computer are getting dangerous it's beating human on everything.")
+    }else {
+        return ("That was a tough battle, but it was a draw.")
+    }
+}
+const gameWinner =playGame();
+
+console.log("Human Score: " + humanScore);
+console.log("Computer Score: "+ computerScore);
+
+console.log(gameWinner)
