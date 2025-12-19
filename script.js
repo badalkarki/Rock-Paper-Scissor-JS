b15df09f1d6df252f9e368ff9cd05ddfb67d6671
Badal karki <128877662+badalkarki@users.noreply.github.com>
2025-12-19 18:32:00 +0545

    Creates a function that receives the human choice
    function with getHumanChoice name is used which receive the input
    rock,paper or scissor. Also uses '.toLowerCase()' to
    make the user input case insensitive.

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
-console.log("hello world!");
+// Function to get the useInput on what they choose.
+function getHumanChoice(userInput){
+    userInput = userInput.toLowerCase();
+    return userInput;
+}
+const userChoice = getHumanChoice(prompt("Choose between rock,paper and scissor"));
+console.log(userChoice)
+
+
