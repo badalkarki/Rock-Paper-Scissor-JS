48ef4aac0c2f917a869196d8a97312d2c4a0c0a9
Badal karki <128877662+badalkarki@users.noreply.github.com>
2025-12-19 18:47:47 +0545

    Creates function to get the computer choice
    used function named getComputerChoice to get the input
    from the computer in the form of number and later used the
    conditional statement to return them as a text based on the
    range of number.
    
    Also to get the random result, Math.random() function was used
    multiplied by 20 to get the number between 0-20;

--- script.js
+++ script.js
@@ -6,4 +6,15 @@ function getHumanChoice(userInput){
 const userChoice = getHumanChoice(prompt("Choose between rock,paper and scissor"));
 console.log(userChoice)
 
-
+// // Function to get the choice of the computer.
+function getComputerChoice(choice){
+    if(choice > 12){
+        return "rock";
+    }else if (choice < 12 && choice > 6){
+        return "paper";
+    }else {
+        return "scissor";
+    }
+}
+const computerChoice = getComputerChoice(Math.random()*20);
+console.log(computerChoice);
